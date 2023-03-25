9357f0e06af7c2415188cb3b2af013f42028b9d8
@wenxihuw <wenxihuw@gmail.com>
2023-03-25 19:05:49 -0400

    Init

--- server.js
+++ server.js
@@ -3,3 +3,8 @@
 import { rps } from '../lib/rpsls.js';
 import { rpsls } from "../lib/rpsls.js";
 import minimist from "minimist";
+import express from "express";
+
+const args = minimist(process.argv.slice(2));
+const PORT = args.port || 5000;
+const app = express();
